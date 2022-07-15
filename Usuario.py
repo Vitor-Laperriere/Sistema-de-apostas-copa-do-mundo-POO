27280e9c0fd95d25e5d76e88b3f9dfc1559b0efb
Kenzo Yves Yamashita Nobre <kenzonobre@gmail.com>
2022-07-14 22:48:21 -0300

    Indentificador adicionado na classe

--- Usuario.py
+++ Usuario.py
@@ -1,17 +1,21 @@
 #	Classe que guarda informacoes sobre o usuario que ira acessar o sistema
 class Usuario:
 
-	def __init__(self, Nome = None, Senha = None, Saldo = 0):
+	def __init__(self, Nome = None, Senha = None, Saldo = 0, Indentificador = -1):
 
 		nome = Nome
 		senha = Senha
-		saldo = saldo
+		saldo = Saldo
+		ind = Indentificador
 
 	def acessar_nome(self):
 		return nome
 
 	def acessar_saldo(self):
 		return saldo
+
+	def acessar_ind(self):
+		return ind
 
 	def adicionar_saldo(self, s):
 		saldo += s
